6b2eb9cd2201e5c5b53aca171313d40cb46813d5
Ankit Shah <ankitshahankit181@gmail.com>
2024-05-26 19:20:07 +0530

    few changes

--- src/Components/AddNote.jsx
+++ src/Components/AddNote.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const AddNote = () => {
-  return <div>AddNote</div>;
+  return (
+    <div className="note new">
+      <textarea
+        placeholder="Type to add a note..."
+        cols="10"
+        rows="8"
+      ></textarea>
+      <div className="note-footer">
+        <small>200 remaining</small>
+        <button className="save">Save</button>
+      </div>
+    </div>
+  );
 };
 
 export default AddNote;
--- src/Components/NotesList.jsx
+++ src/Components/NotesList.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Note from "./Note";
+import AddNote from "./AddNote";
 
 const NotesList = ({ notes }) => {
   return (
     <div className="notes-list">
+      <AddNote />
       {notes.map((note) => (
         <Note key={note.id} note={note} />
       ))}
+
       {/* <Note />
       <Note />
       <Note />
